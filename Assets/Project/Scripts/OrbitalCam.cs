f25713e0c2e6e74fdbc31fa6a8d2c3f931c461e3
Mayuresh Rajwar <mayureshrajwar@devdensolutions.com>
2022-03-26 01:05:08 +0530

    intial commit with project files

---
+++ Assets/Project/Scripts/OrbitalCam.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+public class OrbitalCam : MonoBehaviour
+{
+    public Transform target;
+    public float distance = 5.0f;
+    public float xSpeed = 120.0f;
+    public float ySpeed = 120.0f;
+
+    public float yMinLimit = -20f;
+    public float yMaxLimit = 80f;
+
+    public float rotationSmoothTime = 2f;
+
+    public float sensitivity = 1;
+    public float speed = 15;
+    public float minZoom = 10;
+    public float maxZoom = 30;
+
+    Transform camObj;
+
+    float rotationYAxis = 0.0f;
+    float rotationXAxis = 0.0f;
+
+    float velocityX = 0.0f;
+    float velocityY = 0.0f;
+
+    float zoomLevel;
+    float zoomPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 angles = transform.eulerAngles;
+        rotationYAxis = angles.y;
+        rotationXAxis = angles.x;
+
+        camObj = transform.GetChild(0);
+    }
+
+    void LateUpdate()
+    {
+        if (target)
+        {
+            zoomLevel += Input.mouseScrollDelta.y * sensitivity;
+            zoomLevel = Mathf.Clamp(zoomLevel, minZoom, maxZoom);
+            zoomPosition = Mathf.MoveTowards(zoomPosition, zoomLevel, speed * Time.deltaTime);
+            camObj.position = transform.position + (camObj.forward * zoomPosition);
+
+            if (Input.GetMouseButton(1))
+            {
+                velocityX += xSpeed * Input.GetAxis("Mouse X") * 0.02f;
+                velocityY += ySpeed * Input.GetAxis("Mouse Y") * 0.02f;
+            }
+
+            rotationYAxis += velocityX;
+            rotationXAxis -= velocityY;
+
+            rotationXAxis = ClampAngle(rotationXAxis, yMinLimit, yMaxLimit);
+
+            Quaternion fromRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
+            Quaternion toRotation = Quaternion.Euler(rotationXAxis, rotationYAxis, 0);
+            Quaternion rotation = toRotation;
+                                 
+            Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
+            Vector3 position = rotation * negDistance + target.position;
+
+            transform.rotation = rotation;
+            transform.position = position;
+
+            velocityX = Mathf.Lerp(velocityX, 0, Time.deltaTime * rotationSmoothTime);
+            velocityY = Mathf.Lerp(velocityY, 0, Time.deltaTime * rotationSmoothTime);
+        }
+
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+        {
+            angle += 360F;
+        }
+
+        if (angle > 360F)
+        {
+            angle -= 360F;
+        }
+
+        return Mathf.Clamp(angle, min, max);
+    }
+}
